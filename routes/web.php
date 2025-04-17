c1c3028b46b05224d54935a8c5da17039f0d5c45
chamath <dcpoorna887@gmail.com>
2025-04-17 20:46:16 +0530

    add category page and also create database for category

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,8 @@ require __DIR__.'/auth.php';
 route::get('admin/dashboard', [HomeController::class, 'index'])->
     middleware(['auth', 'admin']);
 
+route::get('view_category', [AdminController::class, 'view_category'])->
+    middleware(['auth', 'admin']);
+
+route::post('add_category', [AdminController::class, 'add_category'])->
+    middleware(['auth', 'admin']);
